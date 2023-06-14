af016d475e576846429f9c93afdc1a81d08251d4
Serenity Parker <sarenity.parker01@gmail.com>
2023-06-14 10:06:10 -0500

    redo

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+function addingEventListener(){
+    const button =document.getElementById("button");
+    button.addEventListener('click', function(){
+        alert('i was clicked')
+
+        console.log("nada");
+    })
+
 }
